6090ab4c60c18aed3238b88bd83bb2077f13a542
Roger Tobias <celticspyder14@gmail.com>
2017-05-17 13:56:36 -0700

    add the finishing functionality to get the story to the point where it will go back to the bios screen.

--- js/textrpg.js
+++ js/textrpg.js
@@ -8,6 +8,7 @@ $(function() {
       $(".office-runaway-form").fadeIn();
     } else if ($(this)[0].classList[0] == "follow") {
       $(".intro-form").fadeOut();
+      $(".resist-form").fadeOut();
       $(".follow-form").fadeIn();
     } else if ($(this)[0].classList[0] == "sit") {
       $(".intro-form").fadeOut();
@@ -16,6 +17,7 @@ $(function() {
       $("#locked-door").text("The Door is Locked").delay(1500).fadeOut();
     } else if ($(this)[0].classList[0] == "left-door-office") {
       $(".office-runaway-form").fadeOut();
+      $(".follow-form").fadeOut();
       $(".office-fight-form").fadeIn();
     } else if ($(this)[0].classList[0] == "resist") {
       $(".office-fight-form").fadeOut();
@@ -24,6 +26,9 @@ $(function() {
       $(".resist-form").fadeOut();
       $(".resist-two-form").fadeIn().delay(2000);
       $(".resist-two-form").fadeOut();
+      $(".operating-room-form").fadeIn();
+    } else if ($(this)[0].classList[0] == "go-in") {
+      $(".follow-form").fadeOut();
       $(".operating-room-form").fadeIn();
     }
   });
