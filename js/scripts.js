85127865184dd17b2ddb42997bd3b8ec6d8faf75
Adrian Agnic, Ethan Luts, Jared Luedtke and Roger Tobias <adrian+ethan+jared+roger@>
2017-05-17 13:13:25 -0700

    add security key hints, styling

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,8 @@ $(function() {
     event.preventDefault();
     if ("think" == $("#start-page-text").val().toLowerCase()) {
       $(".start-page").fadeOut();
-      $(".intro").fadeIn().delay(17000).fadeOut();
-      $("#corePuzzle").delay(17500).fadeIn();
+      $(".intro").fadeIn().delay(1700).fadeOut();
+      $("#corePuzzle").delay(1750).fadeIn();
     } else {
       return;
     };
@@ -15,6 +15,8 @@ $(function() {
 		document.getElementById('motor-div').style.pointerEvents = 'none'; // Make un-clickable
 		document.getElementById('memory-div').style.pointerEvents = 'none';
 		document.getElementById('core-div').style.pointerEvents = 'none';
+    document.getElementById('power-div').style.pointerEvents = 'none';
+
 
     $("div.hub-option").click(function() {
       if ($(this)[0].classList[0] == "visual") {
@@ -23,8 +25,27 @@ $(function() {
       } else if ($(this)[0].classList[0] == "motor") {
 				$(".game-hub").fadeOut();
         $("#motorPuzzle").fadeIn();
+      } else if ($(this)[0].classList[0] == "security") {
+        $(".secKeyInput").fadeToggle();
+      } else if($(this)[0].classList[0] == "security-key") {
+        $(".secKeyHints").text(newGame.secKeyHints)
+        $(".secKeyDisplay").fadeToggle();
+      }else if ($(this)[0].classList[0] == "power") {
+
       }
     });
+
+    $("#secKey").click(function() {
+      var userSecKey = $("input#secKeyInput").val().toLowerCase();
+      if(userSecKey == "iamtherobot") {
+        alert("you win")
+        document.getElementById('power-div').style.pointerEvents = 'auto';
+        $(".secKeyInput").fadeOut();
+        $(".game-hub").fadeIn();
+        $("#power-status").removeClass();
+        $("#power-status").text("ACCESS GRANTED - OFFLINE");
+      };
+    })
 
 		var nextPuzzle = 2;  // multiple puzzle selector (start at 2 in order to keep even with puzzleSelector prototype)
 		$(".next-button").click(function() {
@@ -61,7 +82,7 @@ $(function() {
 				$("#motor-four").text("21")
 			} else if (nextPuzzle == 8) { // motor function puzzle 3
 				$("#right-arm").text("GREEN").removeClass();
-				$("#motor-puzzle-text").text("Data missing from [RIGHT-LEG] memory node: [4] [11-14] [31-14] [21-13-14] [3?-13-??-??]")
+				$("#motor-puzzle-text").text("Data missing from [RIGHT-LEG] memory node: [4] [14] [11-14] [31-14] [21-13-14] [3?-13-??-??]")
 				$("#motor-one").text("[32-13-31-14]")
 				$("#motor-two").text("[32-13-24-14]")
 				$("#motor-three").text("[31-13-12-14]")
@@ -118,7 +139,7 @@ $(function() {
 					$("span#malfunction-count").text(newGame.malfunctionCount + " ");
 					$(".alert").slideDown().delay(3000).slideUp()
 				} else {
-					alert("MALFUNCTION COUNT EXCEEDED KNOWN PARAMETERS. SYSTEM CRASH.");
+					alert("MALFUNCTION COUNT EXCEEDED LIMIT...SYSTEM CRASH IMMENENT");
           for (var i = 1; i > 0; i++) {
             newGame.crash.push(i);
           }
@@ -137,13 +158,16 @@ function Game(malfunctionCount, puzzleState, visualState, motorState, securitySt
   this.coreState = coreState;
   this.powerState = powerState;
   this.crash = [];
+  this.secKeyHints = [];
 }
 
 Game.prototype.solvePuzzle = function(option) {
   if (this.puzzleState == 1) { // core Node puzzle
+    this.secKeyHints.push("There are only 4 words....");
     if (option == 2) {
       this.puzzleState++;
       this.coreState = true;
+      this.secKeyHints.push("The first letter is 'I'...");
       return true;
     } else {
       return false;  // Can we put this at the end of IF/ELSE to dry code?
@@ -151,6 +175,7 @@ Game.prototype.solvePuzzle = function(option) {
   } else if (this.puzzleState == 2) {
     if (option == 3) { // visual node puzzle 1
       this.puzzleState++;
+      this.secKeyHints.push("Each word is 1 character longer than the previous...except the last word.");
       return true;
     } else {
       return false;
@@ -158,6 +183,7 @@ Game.prototype.solvePuzzle = function(option) {
   } else if (this.puzzleState == 3) {
 		if (option == 4) { // visual node puzzle 2
 			this.puzzleState++;
+      this.secKeyHints.push("Each word does not use vowels from other words...");
 			return true;
 		} else {
 			return false;
@@ -165,6 +191,7 @@ Game.prototype.solvePuzzle = function(option) {
 	} else if (this.puzzleState == 4) {
 		if (option == 1) { // visual node puzzle 3
 			this.puzzleState++;
+      this.secKeyHints.push("'O' comes before and after 'B'...");
 			return true;
 		} else {
 			return false;
@@ -173,6 +200,7 @@ Game.prototype.solvePuzzle = function(option) {
 		if (option == 2) { // visual node puzzle 4
 			this.puzzleState++;
 			this.visualState = true;
+      this.secKeyHints.push("The middle letter is 'E'...");
 			return true;
 		} else {
 			return false;
@@ -180,6 +208,7 @@ Game.prototype.solvePuzzle = function(option) {
 	} else if (this.puzzleState == 6) {
 		if (option == 2) { // motor function puzzle 1
 			this.puzzleState++;
+      this.secKeyHints.push("'T' comes after 'O'...");
 			return true;
 		} else {
 			return false;
@@ -187,6 +216,7 @@ Game.prototype.solvePuzzle = function(option) {
 	} else if (this.puzzleState == 7) {
 		if (option == 1) { // motor function puzzle 2
 			this.puzzleState++
+      this.secKeyHints.push("'H' is the middle letter in the last word...");
 			return true;
 		} else {
 			return false;
@@ -202,31 +232,31 @@ Game.prototype.solvePuzzle = function(option) {
 	}
 }
 
-
-Game.prototype.giveSecKey = function(solvePuzzle, puzzleState) {
-  var keyArr = [];
-  if(this.puzzleState == 1) {
-    if(newGame.solvePuzzle() == true) {
-      keyArr.push("puzzle 1 key");
-    }
-    return keyArr;
-  }
-  if(this.puzzleState == 2) {
-    if(newGame.solvePuzzle() == true) {
-      keyArr.push("puzzle 2 key");
-    }
-    return keyArr;
-  }
-  if(this.puzzleState == 3) {
-    if(newGame.solvePuzzle() == true) {
-      keyArr.push("puzzle 3 key");
-    }
-    return keyArr;
-  }
-  if(this.puzzleState == 4) {
-    if(newGame.solvePuzzle() == true) {
-      keyArr.push("puzzle 4 key");
-    }
-    return keyArr;
-  }
-}
+//
+// Game.prototype.giveSecKey = function(solvePuzzle, puzzleState) {
+//   var keyArr = [];
+//   if(this.puzzleState == 1) {
+//     if(newGame.solvePuzzle() == true) {
+//       keyArr.push("puzzle 1 key");
+//     }
+//     return keyArr;
+//   }
+//   if(this.puzzleState == 2) {
+//     if(newGame.solvePuzzle() == true) {
+//       keyArr.push("puzzle 2 key");
+//     }
+//     return keyArr;
+//   }
+//   if(this.puzzleState == 3) {
+//     if(newGame.solvePuzzle() == true) {
+//       keyArr.push("puzzle 3 key");
+//     }
+//     return keyArr;
+//   }
+//   if(this.puzzleState == 4) {
+//     if(newGame.solvePuzzle() == true) {
+//       keyArr.push("puzzle 4 key");
+//     }
+//     return keyArr;
+//   }
+// }
