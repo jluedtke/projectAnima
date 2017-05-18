71ccd8fcecf9a0a3823e85e7b5cffd8eb756a740
Adrian Agnic, Ethan Luts, Jared Luedtke and Roger Tobias <adrian+ethan+jared+roger@>
2017-05-18 09:26:44 -0700

    add the textrpg branch with master branch.

--- js/textrpg.js
+++ js/textrpg.js
@@ -3,47 +3,45 @@ $(function() {
     console.log($(this)[0].classList[0]);
 
     if ($(this)[0].classList[0] == "runaway") {
-      $(".intro-form").fadeOut();
-      $(".sit-form").fadeOut();
+      $(".intro-form").hide();
+      $(".sit-form").hide();
       $(".office-runaway-form").fadeIn();
     } else if ($(this)[0].classList[0] == "follow") {
-      $(".intro-form").fadeOut();
-      $(".office-fight-form").fadeOut();
-      $(".sit-form").fadeOut();
-      $(".resist-form").fadeOut();
+      $(".intro-form").hide();
+      $(".office-fight-form").hide();
+      $(".sit-form").hide();
+      $(".resist-form").hide();
       $(".follow-form").fadeIn();
     } else if ($(this)[0].classList[0] == "sit") {
-      $(".intro-form").fadeOut();
+      $(".intro-form").hide();
       $(".sit-form").fadeIn();
     } else if ($(this)[0].classList[0] == "right-door-office") {
       $("#locked-door").text("The Door is Locked").delay(1500).fadeOut();
     } else if ($(this)[0].classList[0] == "left-door-office") {
-      $(".office-runaway-form").fadeOut();
-      $(".follow-form").fadeOut();
+      $(".office-runaway-form").hide();
+      $(".follow-form").hide();
       $(".office-fight-form").fadeIn();
     } else if ($(this)[0].classList[0] == "resist") {
-      $(".office-fight-form").fadeOut();
+      $(".office-fight-form").hide();
       $(".resist-form").fadeIn();
     } else if ($(this)[0].classList[0] == "resist-two") {
-      $(".resist-form").fadeOut();
+      $(".resist-form").hide();
       $(".resist-two-form").fadeIn().delay(2000);
       $(".resist-two-form").fadeOut();
-      $(".operating-room-form").fadeIn();
+      $(".operating-room-form").delay(2750).fadeIn();
     } else if ($(this)[0].classList[0] == "go-in") {
-      $(".follow-form").fadeOut();
+      $(".follow-form").hide();
       $(".operating-room-form").fadeIn();
-    }
-
-    if ($(this)[0].classList[0] == "escape") {
-      $(".operating-room-form").fadeOut();
-      $(".try-escape").fadeIn();
-      $(".hide-attempt").fadeIn();
+    } else if ($(this)[0].classList[0] == "escape") {
+      $(".operating-room-form").hide();
+      $(".try-escape-form").fadeIn();
+      $(".try-escape-form").fadeOut();
+      $(".hide-attempt-form").fadeIn();
+      $(".hide-attempt-form").fadeOut();
       $(".ending-scene").fadeIn();
-    }
-    else if ($(this)[0].classList[0] == "scan-room") {
-      $(".operating-room-form").fadeOut();
-      $(".hide-attempt").fadeIn();
-      
+    } else if ($(this)[0].classList[0] == "scan-room") {
+      $(".operating-room-form").hide();
+      $(".hide-attempt-form").fadeIn();
       $(".ending-scene").fadeIn();
     }
 
