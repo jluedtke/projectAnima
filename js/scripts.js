2377fa73f16e398c62ca6eddcbad43990387682f
Adrian Agnic, Ethan Luts, Jared Luedtke and Roger Tobias <adrian+ethan+jared+roger@>
2017-05-18 09:27:30 -0700

    add final ending

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,8 @@ $(function() {
     event.preventDefault();
     if ("think" == $("#start-page-text").val().toLowerCase()) {
       $(".start-page").fadeOut();
-      $(".intro").fadeIn().delay(17000).fadeOut();
-      $("#corePuzzle").delay(17500).fadeIn();
+      $(".intro").fadeIn().delay(1700).fadeOut();
+      $("#corePuzzle").delay(1750).fadeIn();
     } else {
       return;
     };
@@ -34,25 +34,27 @@ $(function() {
           $(".secKeyHints").append("<li>" + newGame.secKeyHints[i] + "</li>")
         }
         $(".secKeyDisplay").fadeToggle();
-      }else if ($(this)[0].classList[0] == "memory") {
+      } else if ($(this)[0].classList[0] == "memory") {
         window.open("textrpg.html");
+      } else if ($(this)[0].classList[0] == "power") {
+        console.log($(this)[0].classList[0]);
+        $(".panel").addClass( "power-shadow-green" );
+        $(".panel-body").addClass( "power-shadow-white" );
+        $(".game-hub").show().delay(500).fadeOut(1000);
+        $("#robo-baby").hide().delay(1800).fadeIn(750);
       }
     });
 
     $("#secKey").click(function() {
       var userSecKey = $("input#secKeyInput").val().toLowerCase();
       if(userSecKey == "iamtherobot") {
-        alert("you win")
         document.getElementById('power-div').style.pointerEvents = 'auto';
         $(".secKeyInput").fadeOut();
         $(".game-hub").fadeIn();
         $("#power-status").removeClass();
         $("#power-status").text("ACCESS GRANTED - OFFLINE");
-      }else {
-        alert("WRONG KEY ENTERED. SYSTEM CRASH IMMENENT...")
-        for (var i = 1; i > 0; i++) {
-          newGame.crash.push(i);
-        }
+      } else {
+
       }
     });
 
@@ -61,42 +63,45 @@ $(function() {
 			nextPuzzle += 1;
 			if (nextPuzzle == 3) { // visual node puzzle 2
 				$("#partition-one").text("GREEN").removeClass();
-				$("#visual-puzzle-text").text("21 - 18 - 16 - 15 - 15 - 16 - ??")
-				$("#visual-one").text("15")
-				$("#visual-two").text("16")
-				$("#visual-three").text("22")
-				$("#visual-four").text("18")
+				$("#visual-puzzle-text").text("21 - 18 - 16 - 15 - 15 - 16 - ??");
+				$("#visual-one").text("15");
+				$("#visual-two").text("16");
+				$("#visual-three").text("22");
+				$("#visual-four").text("18");
 			} else if (nextPuzzle == 4) { // visual node puzzle 3
 				$("#partition-two").text("GREEN").removeClass();
-				$("#visual-puzzle-text").text("1 - 2 - 4 - 8 - 16 - 32 - ??")
-				$("#visual-one").text("64")
-				$("#visual-two").text("128")
-				$("#visual-three").text("48")
-				$("#visual-four").text("46")
+				$("#visual-puzzle-text").text("1 - 2 - 4 - 8 - 16 - 32 - ??");
+				$("#visual-one").text("64");
+				$("#visual-two").text("128");
+				$("#visual-three").text("48");
+				$("#visual-four").text("46");
 			} else if (nextPuzzle == 5) { // visual node puzzle 4
 				$("#partition-three").text("GREEN").removeClass();
-				$("#visual-puzzle-text").text("128 - 64 - 32 - 16 - 8 - 4 - ??")
-				$("#visual-one").text("8")
-				$("#visual-two").text("2")
-				$("#visual-three").text("0.5")
-				$("#visual-four").text("4")
+				$("#visual-puzzle-text").text("128 - 64 - 32 - 16 - 8 - 4 - ??");
+				$("#visual-one").text("8");
+				$("#visual-two").text("2");
+				$("#visual-three").text("0.5");
+				$("#visual-four").text("4");
 				nextPuzzle += 1; // to keep in line with motor function puzzle 2
 
 			} else if (nextPuzzle == 7) { // motor function puzzle 2
 				$("#sensor-matrix").text("GREEN").removeClass();
-				$("#motor-puzzle-text").text("Data missing from Prime Values: 1*2*5*11*13*17*19 Recalibrate system with manual placement of prime value.")
-				$("#motor-one").text("7")
-				$("#motor-two").text("9")
-				$("#motor-three").text("15")
-				$("#motor-four").text("21")
+				$("#motor-puzzle-text").text("Data missing from Prime Values: 1*2*5*11*13*17*19 Recalibrate system with manual placement of prime value.");
+				$("#motor-one").text("7");
+				$("#motor-two").text("9");
+				$("#motor-three").text("15");
+				$("#motor-four").text("21");
 			} else if (nextPuzzle == 8) { // motor function puzzle 3
 				$("#right-arm").text("GREEN").removeClass();
-				$("#motor-puzzle-text").text("Data missing from [RIGHT-LEG] memory node: [4] [14] [11-14] [31-14] [21-13-14] [3?-13-??-??]")
-				$("#motor-one").text("[32-13-31-14]")
-				$("#motor-two").text("[32-13-24-14]")
-				$("#motor-three").text("[31-13-12-14]")
-				$("#motor-four").text("[31-13-28-14]")
-			}
+				$("#motor-puzzle-text").text("Data missing from [RIGHT-LEG] memory node: [4] [14] [11-14] [31-14] [21-13-14] [3?-13-??-??]");
+				$("#motor-one").text("[32-13-31-14]");
+				$("#motor-two").text("[32-13-24-14]");
+				$("#motor-three").text("[31-13-12-14]");
+				$("#motor-four").text("[31-13-28-14]");
+        nextPuzzle += 1;
+			} else if (nextPuzzle == 9) {
+        $("#right-leg").text("GREEN").removeClass();
+      }
 
 			$(".output").fadeOut();
 
@@ -114,19 +119,18 @@ $(function() {
       if (newGame.solvePuzzle($(this)[0].classList[0]) == true) {
 				if (newGame.motorState == true) {
 					$(".output").show();
-					$(".exit-button").show();
 					$(".next-button").hide();
+          $(".exit-button").show();
 					$("#partition-five").text("GREEN").removeClass();
 
 
 					$("#motor-status").removeClass();
 					$("#motor-status").text("ACCESS DENIED - ONLINE");
-					$("#memory-status").text("ACCESS GRANTED - OFFLINE");
 					document.getElementById('motor-div').style.pointerEvents = 'none';
 				} else if (newGame.visualState == true && newGame.puzzleState == 6) { // puzzleState when visualState was defined true
 					$(".output").show();
-					$(".exit-button").show();
 					$(".next-button").hide();
+          $(".exit-button").show();
 					$("#partition-five").text("GREEN").removeClass();
 
 
@@ -177,95 +181,50 @@ Game.prototype.solvePuzzle = function(option) {
       this.puzzleState++;
       this.coreState = true;
       return true;
-    } else {
-      return false;  // Can we put this at the end of IF/ELSE to dry code?
     }
   } else if (this.puzzleState == 2) {
     if (option == 3) { // visual node puzzle 1
       this.puzzleState++;
-      this.secKeyHints.push("Each word is 1 character longer than the previous, except the last word which is 2.");
+      this.secKeyHints.push("Each word is 1 character longer than the previous, except the last word which is 2");
       return true;
-    } else {
-      return false;
     }
   } else if (this.puzzleState == 3) {
 		if (option == 4) { // visual node puzzle 2
 			this.puzzleState++;
-      this.secKeyHints.push("Each word does not use vowels from other words, ");
+      this.secKeyHints.push("Each word does not use vowels from other words");
 			return true;
-		} else {
-			return false;
 		}
 	} else if (this.puzzleState == 4) {
 		if (option == 1) { // visual node puzzle 3
 			this.puzzleState++;
-      this.secKeyHints.push("'O' comes before and after 'B', ");
+      this.secKeyHints.push("'O' comes before and after 'B'");
 			return true;
-		} else {
-			return false;
 		}
 	} else if (this.puzzleState == 5) {  // Adrian, this is where your .giveSecKey puzzleState should be at
 		if (option == 2) { // visual node puzzle 4
 			this.puzzleState++;
 			this.visualState = true;
-      this.secKeyHints.push("The middle letter is 'E', ");
+      this.secKeyHints.push("The middle letter is 'E'");
 			return true;
-		} else {
-			return false;
 		}
 	} else if (this.puzzleState == 6) {
 		if (option == 2) { // motor function puzzle 1
 			this.puzzleState++;
-      this.secKeyHints.push("'T' comes after 'O', ");
+      this.secKeyHints.push("'T' comes after 'O'");
 			return true;
-		} else {
-			return false;
 		}
 	} else if (this.puzzleState == 7) {
 		if (option == 1) { // motor function puzzle 2
 			this.puzzleState++
-      this.secKeyHints.push("'H' is the middle letter in the last word, ");
+      this.secKeyHints.push("'H' is the middle letter in the last word");
 			return true;
-		} else {
-			return false;
 		}
 	} else if (this.puzzleState == 8) { // giveSecKey for Motor Function
 		if (option == 3) { // motor function puzzle 3
 			this.puzzleState++;
 			this.motorState = true;
-      this.secKeyHints.push("The first letter is 'I', ");
+      this.secKeyHints.push("The first letter is 'I'");
 			return true;
-		} else {
-			return false;
 		}
 	}
 }
-
-//
-// Game.prototype.giveSecKey = function(solvePuzzle, puzzleState) {
-//   var keyArr = [];
-//   if(this.puzzleState == 1) {
-//     if(newGame.solvePuzzle() == true) {
-//       keyArr.push("puzzle 1 key");
-//     }
-//     return keyArr;
-//   }
-//   if(this.puzzleState == 2) {
-//     if(newGame.solvePuzzle() == true) {
-//       keyArr.push("puzzle 2 key");
-//     }
-//     return keyArr;
-//   }
-//   if(this.puzzleState == 3) {
-//     if(newGame.solvePuzzle() == true) {
-//       keyArr.push("puzzle 3 key");
-//     }
-//     return keyArr;
-//   }
-//   if(this.puzzleState == 4) {
-//     if(newGame.solvePuzzle() == true) {
-//       keyArr.push("puzzle 4 key");
-//     }
-//     return keyArr;
-//   }
-// }
